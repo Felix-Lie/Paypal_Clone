626daedd10f6fe4ffe71f13dc2c91738fbe4dee6
Felix lie <felixlie19@gmail.com>
2022-01-18 22:52:33 -0800

    Added Media Queries

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 document.querySelectorAll('.logo').forEach((logo) => {
   logo.addEventListener('click', () => {
     document.querySelector('.front-page').style.display = 'block';
-  });
-  logo.addEventListener('click', () => {
     document.querySelector('.login-page').style.display = 'none';
-  });
-  logo.addEventListener('click', () => {
     document.querySelector('.signup-page').style.display = 'none';
   });
 });
@@ -14,11 +10,7 @@ document.querySelectorAll('.logo').forEach((logo) => {
 document.querySelectorAll('.login').forEach((loginBtn) => {
   loginBtn.addEventListener('click', () => {
     document.querySelector('.front-page').style.display = 'none';
-  });
-  loginBtn.addEventListener('click', () => {
     document.querySelector('.login-page').style.display = 'block';
-  });
-  loginBtn.addEventListener('click', () => {
     document.querySelector('.signup-page').style.display = 'none';
   });
 });
@@ -26,11 +18,7 @@ document.querySelectorAll('.login').forEach((loginBtn) => {
 document.querySelectorAll('.signup').forEach((signupBtn) => {
   signupBtn.addEventListener('click', () => {
     document.querySelector('.front-page').style.display = 'none';
-  });
-  signupBtn.addEventListener('click', () => {
     document.querySelector('.login-page').style.display = 'none';
-  });
-  signupBtn.addEventListener('click', () => {
     document.querySelector('.signup-page').style.display = 'flex';
   });
 });
@@ -39,17 +27,53 @@ document.querySelectorAll('.signup').forEach((signupBtn) => {
 // Navigation
 const dropdownItems = document.querySelectorAll('.dropdown-hover');
 
-dropdownItems.forEach((dropdownItem) => {
-  dropdownItem.addEventListener('mouseover', () => {
-    dropdownItem.lastElementChild.style.cssText =
-      'opacity:1; visibility: visible';
-    document.querySelector('.navbar-wrapper').style.background =
-      'linear-gradient(to right, #066399, #2f8fdf, #066399)';
+if (window.innerWidth < 1000) {
+  const menuIcon = document.querySelector('.menu');
+  const navbar = document.querySelector('.navbar');
+
+  menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+
+    if (!navbar.classList.contains('change')) {
+      document.querySelectorAll('.nav-dropdown').forEach((dropdown) => {
+        dropdown.style.left = '-20rem';
+      });
+    }
   });
-  dropdownItem.addEventListener('mouseout', () => {
-    dropdownItem.lastElementChild.style.cssText =
-      'opacity:0; visibility: hidden';
-    document.querySelector('.navbar-wrapper').style.background = 'none';
+
+  document.querySelectorAll('.show-dropdown').forEach((link) => {
+    link.addEventListener('click', () => {
+      link.nextElementSibling.style.left = '0';
+    });
   });
+
+  document.querySelectorAll('.dropdown-heading-link').forEach((headingLink) => {
+    headingLink.addEventListener('click', () => {
+      headingLink.parentElement.parentElement.style.left = '-20rem';
+    });
+  });
+} else {
+  dropdownItems.forEach((dropdownItem) => {
+    dropdownItem.addEventListener('mouseover', () => {
+      dropdownItem.lastElementChild.style.cssText =
+        'opacity: 1; visibility: visible';
+      document.querySelector('.navbar-wrapper').style.background =
+        'linear-gradient(to right, #066399, #2f8fdf, #066399)';
+      dropdownItem.firstElementChild.firstElementChild.style.transform =
+        'rotate(180deg)';
+    });
+    dropdownItem.addEventListener('mouseout', () => {
+      dropdownItem.lastElementChild.style.cssText =
+        'opacity: 0; visibility: hidden';
+      document.querySelector('.navbar-wrapper').style.background = 'none';
+      dropdownItem.firstElementChild.firstElementChild.style.transform =
+        'rotate(0)';
+    });
+  });
+}
+
+window.addEventListener('resize', () => {
+  window.location.reload();
 });
+
 // End of Navigation
